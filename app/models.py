a49d831cec4106c20bddbc4482663da1c5ca8303
Josip Igrec <me@allypost.net>
2020-02-08 20:48:26 +0100

    Allow model to JSON serialization with hidden fields

--- app/models.py
+++ app/models.py
@@ -1,17 +1,35 @@
 from datetime import datetime
+from typing import Dict, Any, Optional
 
 from flask_sqlalchemy import Model
-
+from sqlalchemy import Table
+from sqlalchemy.ext.declarative import declared_attr
 from app.db import db
 
 
 class BaseModel(Model):
+    __table__: Table
+
     id = db.Column(db.Integer, primary_key=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
 
-    def __repr__(self):
+    __hidden = set()
+
+    def __repr__(self) -> str:
         return "<%s %r>" % (type(self).__name__, self.id)
+
+    @declared_attr
+    def __tablename__(self) -> str:
+        return self.__name__
+
+    def to_dict(self) -> Dict[str, Any]:
+        """Serializes only column data."""
+        return {
+            c.name: getattr(self, c.name)
+            for c in self.__table__.columns
+            if c.name not in self.__hidden
+        }
 
 
 BaseModel = db.make_declarative_base(BaseModel, db.metadata)
@@ -19,6 +37,7 @@ BaseModel = db.make_declarative_base(BaseModel, db.metadata)
 
 class User(BaseModel):
     __tablename__ = "users"
+    __hidden = {"password"}
 
     username = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(511), nullable=False)
@@ -26,6 +45,7 @@ class User(BaseModel):
 
 class Image(BaseModel):
     __tablename__ = "images"
+    __hidden = {"fs_path"}
 
     name = db.Column(db.String(255), nullable=False, unique=True)
     fs_path = db.Column(db.String(511), nullable=False)
@@ -34,6 +54,7 @@ class Image(BaseModel):
 
 class Tweet(BaseModel):
     __tablename__ = "tweets"
+    __hidden = {"poster_id", "image_id"}
 
     text = db.Column(db.Text, nullable=False)
 
@@ -41,4 +62,4 @@ class Tweet(BaseModel):
     poster = db.relationship("User", backref=db.backref("users", lazy=True))
 
     image_id = db.Column(db.Integer, db.ForeignKey("images.id"))
-    image = db.relationship("Image", backref=db.backref("tweets", lazy=True))
+    image = db.relationship("Image", backref=db.backref("images", lazy=True))
