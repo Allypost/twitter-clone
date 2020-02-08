65f2461bc629f3358a0bcbf0c18c2dffe379395a
Josip Igrec <me@allypost.net>
2020-02-08 20:49:33 +0100

    Add Vuex module compile-time autoloading

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,9 +3,28 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+const requireAll =
+        (r) =>
+          Object.fromEntries(
+            r
+              .keys()
+              .map(
+                (mpath, ...args) => {
+                  const result = r(mpath, ...args).default;
+                  const name =
+                          mpath
+                            .replace(/(?:^[./]*\/|\.[^.]+$)/g, "") // Trim
+                            .replace(/\//g, "_") // Relace '/'s by '_'s
+                  ;
+
+                  return [ name, result ];
+                },
+              ),
+          );
+
 export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: requireAll(require.context("./modules", false, /\.js$/)),
 });
