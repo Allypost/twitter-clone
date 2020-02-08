21ee3023ab0b00c15958962bdcc941a9afbb8f2e
Josip Igrec <me@allypost.net>
2020-02-08 20:52:00 +0100

    Move global styles to separate file

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,25 +9,9 @@
   </div>
 </template>
 
+<style lang="scss" src="./assets/styles/global.scss"></style>
 <style lang="scss">
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2C3E50;
-  }
-
   #nav {
     padding: 30px;
-
-    a {
-      font-weight: bold;
-      color: #2C3E50;
-
-      &.router-link-exact-active {
-        color: #42B983;
-      }
-    }
   }
 </style>
